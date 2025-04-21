33af58b156c1e564c73347453c165ad3594174a5
takashi <tiranotirano888@gmail.com>
2025-04-21 15:36:03 +0900

    Update Formula: Add resources and update Python version

--- Formula/crules.rb
+++ Formula/crules.rb
@@ -1,13 +1,23 @@
 class Crules < Formula
   include Language::Python::Virtualenv
 
-  desc "Project rules management CLI tool"
+  desc "プロジェクトのルール管理を効率的に行うためのツール"
   homepage "https://github.com/tirano-tirano/crules"
-  url "https://github.com/tirano-tirano/crules/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "99441c35bb8dc8dd322e707ab65b9d0fc29239dcf4bddce068a8d33a4a0d15ff"
+  url "https://github.com/tirano-tirano/crules/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   license "MIT"
 
-  depends_on "python@3.10"
+  depends_on "python@3.9"
+
+  resource "jinja2" do
+    url "https://files.pythonhosted.org/packages/7a/ff/75c28576a1d900e87eb6335b063fab47a8ef3c8b4d88524c4bf78f670cce/Jinja2-3.1.2.tar.gz"
+    sha256 "31351a702a408a9e7595a8fc6150fc3f43bb6bf7e319770cbc0db9df9437e852"
+  end
+
+  resource "markupsafe" do
+    url "https://files.pythonhosted.org/packages/6d/7c/59a3248f411813f8ccba92a55feaac4bf360d29e2ff05ee7d8e1ef2d7dbf/MarkupSafe-2.1.3.tar.gz"
+    sha256 "af598ed32d6ae86f1b747b82783958b1a4ab8f617b06fe68795c7f026abbdcad"
+  end
 
   def install
     virtualenv_install_with_resources
